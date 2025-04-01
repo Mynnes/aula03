fafe0c4f1ebc2b549b5ddec67dd70992af9932a3
Jamynes <jamynesaraujo10@gmail.com>
2025-04-01 14:42:22 -0300

    segundo commit

---
+++ EX2.py
@@ -0,0 +1,11 @@
+number1 = int(input("digite o primeiro número"))
+number2 = int(input("digite o segundo número"))
+
+soma = number1 + number2
+divisao = number1/number2
+m = number1 * number2
+s = number1 - number2
+print(f"a soma de {number1} com {number2} é")
+print(f"a divisão de {number1} com {number2} é")
+print(f"a multiplicação de {number1} com {number2} é")
+print(f"a subtração de {number1} com {number2} é")
--- atividade.py
+++ atividade.py
@@ -1,4 +1,9 @@
 nome = input(f"digite seu nome ")
-idade = input(f"digite sua idade ")
+idade = int(input(f"digite sua idade "))
 money = float(input("digite seu salário "))
+p = float(input(f"qual é a porcentagem"))
+
+conta = (money + p)/100
+soma = (conta + money)
 print(f" o seu nome é {nome}, sua idade é {idade} seu {money}")
+
